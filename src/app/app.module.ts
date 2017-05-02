084f91f2ec6c957a79cd4c7f018db910bf45a543
flaviy <lfiecbr1>
2017-05-02 18:07:27 +0200

    added routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,29 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
+import {RouterModule} from '@angular/router';
 import {HttpModule} from '@angular/http';
 
 import {AppComponent} from './app.component';
 import {SortPipe} from './pipes/app.sort';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
     declarations: [
         SortPipe,
         AppComponent,
+        FormComponent,
     ],
     imports: [
         BrowserModule,
         FormsModule,
-        HttpModule
+        HttpModule,
+        RouterModule.forRoot([
+            {
+                path: 'simple-form',
+                component : FormComponent
+            }
+        ])
     ],
     providers: [],
     bootstrap: [AppComponent]
